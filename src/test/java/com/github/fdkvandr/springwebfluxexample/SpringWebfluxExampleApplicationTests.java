ed6eba89ff9da84361c2de99842a27b5a509e5e2
Andrey Fedyakov <ya.andreyfedyakov@yandex.ru>
2023-09-11 23:06:01 +0300

    feat: initial commit

---
+++ src/test/java/com/github/fdkvandr/springwebfluxexample/SpringWebfluxExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fdkvandr.springwebfluxexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
